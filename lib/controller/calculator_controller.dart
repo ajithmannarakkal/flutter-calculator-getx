696e49ede4e79bbbc4e5d354aa9b781d2f3bcb9d
ajithmannarakkal <ajith.m@hexeam.co.in>
2026-04-20 20:22:25 +0530

    Implemented input handling logic

--- lib/controller/calculator_controller.dart
+++ lib/controller/calculator_controller.dart
@@ -2,4 +2,14 @@ import 'package:get/get.dart';
 
 class CalculatorController extends GetxController{
 
+  var input="0".obs;
+  void appendValues(String value){
+    if(input.value =="0"){
+      input.value=value;
+    }
+    else{
+      input.value+=value;
+    }
+  }
+
 }
--- lib/view/calculator_screen.dart
+++ lib/view/calculator_screen.dart
@@ -11,27 +11,27 @@ class CalculatorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Calculator')),
+      appBar: AppBar(title:  Text('Calculator')),
       body: Column(children: [
         Expanded(
           child: Container(alignment: Alignment.bottomRight,
           padding: EdgeInsets.all(20),
-          child: Text('0'),
+          child: Obx(()=>Text(controller.input.value)),
           ),
         ),
         GridView.count(crossAxisCount: 4,shrinkWrap: true,
         children: [
-          TextButton(onPressed: (){}, child: Text('7')),
-          TextButton(onPressed: (){}, child: Text('8')),
-          TextButton(onPressed: (){}, child: Text('9')),
+          TextButton(onPressed: ()=>controller.appendValues("7"), child: Text('7')),
+          TextButton(onPressed: ()=>controller.appendValues("8"), child: Text('8')),
+          TextButton(onPressed: ()=>controller.appendValues("9"), child: Text('9')),
           TextButton(onPressed: (){}, child: Text('X')),
-          TextButton(onPressed: (){}, child: Text('4')),
-          TextButton(onPressed: (){}, child: Text('5')),
-          TextButton(onPressed: (){}, child: Text('6')),
+          TextButton(onPressed: ()=>controller.appendValues("4"), child: Text('4')),
+          TextButton(onPressed: ()=>controller.appendValues("5"), child: Text('5')),
+          TextButton(onPressed: ()=>controller.appendValues("6"), child: Text('6')),
           TextButton(onPressed: (){}, child: Text('-')),
-          TextButton(onPressed: (){}, child: Text('1')),
-          TextButton(onPressed: (){}, child: Text('2')),
-          TextButton(onPressed: (){}, child: Text('3')),
+          TextButton(onPressed: ()=>controller.appendValues("1"), child: Text('1')),
+          TextButton(onPressed: ()=>controller.appendValues("2"), child: Text('2')),
+          TextButton(onPressed: ()=>controller.appendValues("3"), child: Text('3')),
           TextButton(onPressed: (){}, child: Text('+'))
         ],
         )
