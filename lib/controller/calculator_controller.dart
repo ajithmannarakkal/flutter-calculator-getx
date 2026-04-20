aa0c03e9163726a9563df46a4d3bb16d5c0eab21
ajithmannarakkal <ajith.m@hexeam.co.in>
2026-04-20 20:40:26 +0530

    Added basic calculation functionality

--- lib/controller/calculator_controller.dart
+++ lib/controller/calculator_controller.dart
@@ -3,6 +3,37 @@ import 'package:get/get.dart';
 class CalculatorController extends GetxController{
 
   var input="0".obs;
+
+  var firstNumber=0.0;
+  var operator="";
+
+  void setOperator(String op){
+    firstNumber=double.parse(input.value);
+    operator=op;
+    input.value="";
+  }
+
+  void calculate(){
+    double secondNumber=double.parse(input.value);
+    double result=0;
+    switch(operator){
+      case "+":
+        result=firstNumber+secondNumber;
+        break;
+      case "-":
+        result=firstNumber-secondNumber;
+        break;
+      case "*":
+        result=firstNumber*secondNumber;
+        break;
+      case "/":
+        result=firstNumber/secondNumber;
+        break;
+    }
+    input.value=result.toString();
+  }
+
+
   void appendValues(String value){
     if(input.value =="0"){
       input.value=value;
--- lib/view/calculator_screen.dart
+++ lib/view/calculator_screen.dart
@@ -11,31 +11,65 @@ class CalculatorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:  Text('Calculator')),
-      body: Column(children: [
-        Expanded(
-          child: Container(alignment: Alignment.bottomRight,
-          padding: EdgeInsets.all(20),
-          child: Obx(()=>Text(controller.input.value)),
-          ),
-        ),
-        GridView.count(crossAxisCount: 4,shrinkWrap: true,
+      appBar: AppBar(title: Text('Calculator')),
+      body: Column(
         children: [
-          TextButton(onPressed: ()=>controller.appendValues("7"), child: Text('7')),
-          TextButton(onPressed: ()=>controller.appendValues("8"), child: Text('8')),
-          TextButton(onPressed: ()=>controller.appendValues("9"), child: Text('9')),
-          TextButton(onPressed: (){}, child: Text('X')),
-          TextButton(onPressed: ()=>controller.appendValues("4"), child: Text('4')),
-          TextButton(onPressed: ()=>controller.appendValues("5"), child: Text('5')),
-          TextButton(onPressed: ()=>controller.appendValues("6"), child: Text('6')),
-          TextButton(onPressed: (){}, child: Text('-')),
-          TextButton(onPressed: ()=>controller.appendValues("1"), child: Text('1')),
-          TextButton(onPressed: ()=>controller.appendValues("2"), child: Text('2')),
-          TextButton(onPressed: ()=>controller.appendValues("3"), child: Text('3')),
-          TextButton(onPressed: (){}, child: Text('+'))
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomRight,
+              padding: EdgeInsets.all(20),
+              child: Obx(() => Text(controller.input.value)),
+            ),
+          ),
+          GridView.count(
+            crossAxisCount: 4,
+            shrinkWrap: true,
+            children: [
+              TextButton(
+                onPressed: () => controller.appendValues("7"),
+                child: Text('7'),
+              ),
+              TextButton(
+                onPressed: () => controller.appendValues("8"),
+                child: Text('8'),
+              ),
+              TextButton(
+                onPressed: () => controller.appendValues("9"),
+                child: Text('9'),
+              ),
+              TextButton(onPressed: () =>controller.setOperator("*"), child: Text('X')),
+              TextButton(
+                onPressed: () => controller.appendValues("4"),
+                child: Text('4'),
+              ),
+              TextButton(
+                onPressed: () => controller.appendValues("5"),
+                child: Text('5'),
+              ),
+              TextButton(
+                onPressed: () => controller.appendValues("6"),
+                child: Text('6'),
+              ),
+              TextButton(onPressed: ()=>controller.setOperator("-"), child: Text('-')),
+              TextButton(
+                onPressed: () => controller.appendValues("1"),
+                child: Text('1'),
+              ),
+              TextButton(
+                onPressed: () => controller.appendValues("2"),
+                child: Text('2'),
+              ),
+              TextButton(
+                onPressed: () => controller.appendValues("3"),
+                child: Text('3'),
+              ),
+              TextButton(onPressed: () =>controller.setOperator("+"), child: Text('+')),
+              TextButton(onPressed: () =>controller.calculate(), child: Text('=')),
+
+            ],
+          ),
         ],
-        )
-      ],),
+      ),
     );
   }
 }
