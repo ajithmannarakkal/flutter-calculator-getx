d323e473de29339f326dcd47c03584c11925517c
ajithmannarakkal <ajith.m@hexeam.co.in>
2026-04-20 19:59:20 +0530

    Added calculator controller and connected to UI

---
+++ lib/view/calculator_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controller/calculator_controller.dart';
+
+class CalculatorScreen extends StatelessWidget {
+  final controller = Get.put(CalculatorController());
+
+  CalculatorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Calculator')),
+      body: const Center(child: Text('Calculator Screen')),
+    );
+  }
+}
