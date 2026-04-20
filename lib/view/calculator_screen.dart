7a43234210594a3e6a72e681a806e62c00f06a42
ajithmannarakkal <ajith.m@hexeam.co.in>
2026-04-20 20:10:19 +0530

    Implemented basic calculator UI layout

--- lib/view/calculator_screen.dart
+++ lib/view/calculator_screen.dart
@@ -12,7 +12,30 @@ class CalculatorScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Calculator')),
-      body: const Center(child: Text('Calculator Screen')),
+      body: Column(children: [
+        Expanded(
+          child: Container(alignment: Alignment.bottomRight,
+          padding: EdgeInsets.all(20),
+          child: Text('0'),
+          ),
+        ),
+        GridView.count(crossAxisCount: 4,shrinkWrap: true,
+        children: [
+          TextButton(onPressed: (){}, child: Text('7')),
+          TextButton(onPressed: (){}, child: Text('8')),
+          TextButton(onPressed: (){}, child: Text('9')),
+          TextButton(onPressed: (){}, child: Text('X')),
+          TextButton(onPressed: (){}, child: Text('4')),
+          TextButton(onPressed: (){}, child: Text('5')),
+          TextButton(onPressed: (){}, child: Text('6')),
+          TextButton(onPressed: (){}, child: Text('-')),
+          TextButton(onPressed: (){}, child: Text('1')),
+          TextButton(onPressed: (){}, child: Text('2')),
+          TextButton(onPressed: (){}, child: Text('3')),
+          TextButton(onPressed: (){}, child: Text('+'))
+        ],
+        )
+      ],),
     );
   }
 }
